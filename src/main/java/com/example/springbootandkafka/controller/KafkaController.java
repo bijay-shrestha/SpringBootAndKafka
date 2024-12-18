c0a4fa2e5336bd1e42879a5ba3f5601ca6c86f4e
Bijay Shrestha <7867129+bijay-shrestha@users.noreply.github.com>
2024-12-18 10:42:18 -0800

    Added Kafka Controller and application.properties attributes

---
+++ src/main/java/com/example/springbootandkafka/controller/KafkaController.java
@@ -0,0 +1,27 @@
+package com.example.springbootandkafka.controller;
+
+import com.example.springbootandkafka.service.KafkaProducerService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/kafka")
+public class KafkaController {
+
+    private final KafkaProducerService kafkaProducerService;
+
+    @Autowired
+    public KafkaController(KafkaProducerService kafkaProducerService) {
+        this.kafkaProducerService = kafkaProducerService;
+    }
+
+    @PostMapping("/send")
+    public String sendMessage(@RequestBody String message){
+        kafkaProducerService.sendMessage(message);
+        return "Message sent to Kafka";
+    }
+
+}
