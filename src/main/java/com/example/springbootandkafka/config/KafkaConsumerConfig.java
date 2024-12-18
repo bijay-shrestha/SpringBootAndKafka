5d94a558f389288154ac2ad6e8589f7e339690ea
Bijay Shrestha <7867129+bijay-shrestha@users.noreply.github.com>
2024-12-18 10:37:25 -0800

    Added Kafka Producer and Consumer Configs

---
+++ src/main/java/com/example/springbootandkafka/config/KafkaConsumerConfig.java
@@ -0,0 +1,47 @@
+package com.example.springbootandkafka.config;
+
+import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.apache.kafka.common.serialization.StringDeserializer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.annotation.EnableKafka;
+import org.springframework.kafka.core.DefaultKafkaConsumerFactory;
+import org.springframework.kafka.listener.MessageListener;
+import org.springframework.kafka.core.ConsumerFactory;
+import org.springframework.kafka.listener.MessageListenerContainer;
+import org.springframework.kafka.listener.ConcurrentMessageListenerContainer;
+
+import java.util.HashMap;
+import java.util.Map;
+
+
+@EnableKafka
+@Configuration
+public class KafkaConsumerConfig {
+
+    @Bean
+    public MessageListener<String, String> messageListener() {
+        return new MessageListener<String, String>() {
+            @Override
+            public void onMessage(ConsumerRecord<String, String> record) {
+                System.out.println("Consumed message: " + record.value());
+            }
+        };
+    }
+
+    @Bean
+    public ConcurrentMessageListenerContainer<String, String> messageListenerContainer() {
+        return new ConcurrentMessageListenerContainer<>(consumerFactory(), null
+        );
+    }
+
+    private ConsumerFactory<String, String> consumerFactory() {
+        Map<String, Object> configProps = new HashMap<>();
+        configProps.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
+        configProps.put(ConsumerConfig.GROUP_ID_CONFIG, "test-group");
+        configProps.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
+        configProps.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
+        return new DefaultKafkaConsumerFactory<>(configProps);
+    }
+}
